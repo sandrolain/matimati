c33ded524234e93ad4fe0160d9e3aee861b52022
Sandro Lain <sandrolain@MBP-di-Sandro.station>
2020-06-03 06:25:38 +0200

    First usable version

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,66 @@
 import { HtnaMatiButton } from "htna-components";
-import { createElement } from "htna-tools";
+import { createElement, AudioItem, createAudioContext } from "htna-tools";
 
 HtnaMatiButton.register();
 
 
-const colors = [
-  "#FF0099",
-  "#00CC66",
-  "#0066FF",
-  "#FF9900"
-];
+// const colors = [
+//   "#FF0099",
+//   "#00CC66",
+//   "#0066FF",
+//   "#FF9900"
+// ];
 
-const $app = document.createElement("div");
+const vocalColor = "#0066FF";
+const consonantColor = "#FF0099";
+const numberColor = "#FF9900";
 
-for(let i = 1; i <= 10; i++) {
-  $app.appendChild(createElement("htna-mati-button", {
-    color: colors[i % colors.length],
-    click: () => {
-      alert(i);
-    }
-  }, [i.toString()]));
-}
+
+const $letters = createElement("div", {
+  id: "letters"
+});
+
+const $numbers = createElement("div", {
+  id: "numbers"
+});
+
+const $app = createElement("div", {
+  id: "app"
+}, [$letters, $numbers]);
+
+
+const audioContext = createAudioContext();
+const audios: Record<string, AudioItem> = {};
 
 
 const letters = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z".split(",");
+const vocals = "A,E,I,O,U".split(",");
 
-for(let i = 1, len = letters.length; i < len; i++) {
-  $app.appendChild(createElement("htna-mati-button", {
-    color: colors[i % colors.length],
+for(let i = 0, len = letters.length; i < len; i++) {
+  const letter = letters[i];
+  (async (): Promise<void> => {
+    audios[letter] = await AudioItem.createFromURL(audioContext, `./sounds/${letter}.mp3`);
+  })();
+  $letters.appendChild(createElement("htna-mati-button", {
+    size: "44px",
+    color: vocals.includes(letter) ? vocalColor : consonantColor,
     click: () => {
-      alert(letters[i]);
+      audios[letter].play();
     }
   }, [letters[i]]));
+}
+
+for(let i = 1; i <= 10; i++) {
+  (async (): Promise<void> => {
+    audios[i.toString()] = await AudioItem.createFromURL(audioContext, `./sounds/${i}.mp3`);
+  })();
+  $numbers.appendChild(createElement("htna-mati-button", {
+    size: "38px",
+    color: numberColor,
+    click: () => {
+      audios[i.toString()].play();
+    }
+  }, [i.toString()]));
 }
 
 
